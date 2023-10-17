90f5969a6e9eddc5c91068b2fb9ff6730d71a979
nastya <nastenka29001@gmail.com>
2023-10-17 12:29:00 +0300

    Добавьте файлы проекта.

---
+++ 3.7.1/3.7.1/3.7.1.cpp
@@ -0,0 +1,36 @@
+﻿// 3.7.1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#define MODE 1
+
+#ifdef MODE
+#if MODE == 1
+int add(int a, int b)
+{
+    return a + b;
+   }
+
+#endif
+
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+#if MODE == 0
+    std::cout << "Работаю в боевом режиме\n";
+#elif MODE == 1
+    int a, b;
+    std::cout << "Введите число 1: ";
+    std::cin >> a;
+    std::cout << "Введите число 2: ";
+    std::cin >> b;
+    std::cout << "Результат сложения: " << add(a, b);
+#else
+    std::cout << "Неизвестный режим. Завершение работы\n";
+#endif
+
+}
+
+#else
+#error it is necessary to define MODE
+#endif
